c64400d6efc54c198e57c2c01ebe13608689a5e4
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-04 15:55:25 +0300

    Add ErrorFormatter

---
+++ lib/core/exception_handling/error_formatter.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+
+class ErrorFormatter {
+  ErrorFormatter._();
+
+  static String format(Exception exception) {
+    if (exception is SocketException) {
+      return 'Please check your internet connection';
+    }
+
+    if (exception is HttpException) {
+      return exception.message;
+    }
+
+    if (exception is PlatformException) {
+      return exception.message ?? exception.code;
+    }
+
+    return 'Unexpected $exception';
+  }
+}
