3f40fbd2d119ac56bb01a6180064e15b9710feec
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-04 13:55:01 +0300

    Add app theme to reflect web colors

---
+++ lib/core/theme/dark_app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:kite/core/theme/app_theme.dart';
+
+final ThemeData darkTheme = _DarkAppTheme().theme();
+
+@immutable
+class _DarkAppTheme extends AppTheme {
+  @override
+  ThemeData theme() {
+    final colorScheme = ColorScheme.fromSeed(
+      brightness: Brightness.dark,
+      seedColor: const Color(0xFFFFB319),
+      primaryFixed: const Color(0xFFFFB319),
+      surface: const Color(0xFF1A202C),
+      onSurface: const Color(0xFFE2E8F0),
+    );
+
+    return ThemeData(
+      brightness: Brightness.dark,
+      colorScheme: colorScheme,
+    );
+  }
+}
---
+++ lib/core/theme/light_app_theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:kite/core/theme/app_theme.dart';
+
+final ThemeData lightTheme = _LightAppTheme().theme();
+
+@immutable
+class _LightAppTheme extends AppTheme {
+  @override
+  ThemeData theme() {
+    final colorScheme = ColorScheme.fromSeed(
+      brightness: Brightness.light,
+      seedColor: const Color(0xFFFFB319),
+      primaryFixed: const Color(0xFFFFB319),
+      surface: Colors.white,
+      onSurface: const Color(0xFF1E2939),
+    );
+
+    return ThemeData(
+      brightness: Brightness.light,
+      colorScheme: colorScheme,
+    );
+  }
+}
