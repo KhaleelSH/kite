3a622c1476bbd80e3629898cb9f63d45845c2e33
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-10 13:37:19 +0300

    Add StoryArticleImageSection

---
+++ lib/ui/story/widget/story_article_image_section.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:kite/model/article.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class StoryArticleImageSection extends StatelessWidget {
+  const StoryArticleImageSection({super.key, required this.article});
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => launchUrl(Uri.parse(article.link), mode: LaunchMode.inAppBrowserView),
+      child: Stack(
+        children: [
+          Image.network(
+            article.image,
+            width: double.maxFinite,
+            height: 250,
+            fit: BoxFit.cover,
+            errorBuilder: (context, error, stackTrace) {
+              return Container(
+                height: 250,
+                color: Theme.of(context).colorScheme.surfaceBright,
+                child: const Center(child: Icon(Icons.image_not_supported)),
+              );
+            },
+          ),
+          if (article.imageCaption.isNotEmpty)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(8),
+                color: Colors.black.withAlpha(128),
+                child: Text(
+                  article.imageCaption,
+                  style: Theme.of(
+                    context,
+                  ).textTheme.bodySmall?.copyWith(color: Colors.white, fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
