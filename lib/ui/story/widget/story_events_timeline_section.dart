be36c4c9a647c079627b15e614fb0dfcbf9447e4
Khaleel Shaheen <khaleel_sh@hotmail.com>
2025-05-10 13:12:41 +0300

    Add StoryEventsTimelineSection

---
+++ lib/ui/story/widget/story_events_timeline_section.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class StoryEventsTimelineSection extends StatelessWidget {
+  const StoryEventsTimelineSection({super.key, required this.timeline});
+
+  final List<String> timeline;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (final (index, event) in timeline.indexed)
+          IntrinsicHeight(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  children: [
+                    Badge.count(
+                      count: index + 1,
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      textStyle: Theme.of(context).textTheme.titleMedium,
+                      textColor: Theme.of(context).colorScheme.onSecondary,
+                    ),
+                    if (index != timeline.length - 1)
+                      Expanded(
+                        child: Container(
+                          width: 2,
+                          margin: const EdgeInsets.symmetric(vertical: 12),
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                  ],
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text(
+                        event.split(':: ').first,
+                        style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(event.split(':: ')[1]),
+                      if (index != timeline.length - 1) const SizedBox(height: 24),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+}
